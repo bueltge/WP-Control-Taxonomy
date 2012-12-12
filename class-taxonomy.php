3281d3be1b6633e360c97826223c44c265de7e75
Frank Bültge <frank@bueltge.de>
2012-12-12 10:04:01 +0100

    update for wp 3.5 and other new features

--- class-taxonomy.php
+++ class-taxonomy.php
@@ -7,10 +7,36 @@
  * @author   Frank Bültge <frank@bueltge.de>
  */
 
+// avoid direct calls to this file, because now WP core and framework has been used.
+if ( ! function_exists( 'add_filter' ) ) {
+	echo "Hi there! I'm just a part of plugin, not much I can do when called directly.";
+	exit;
+}
+
 class Wp_Control_Taxonomy {
+	
+	/**
+	 * The Class Object
+	 */
+	protected static $classobj = NULL;
+	
+	/**
+	 * Initialize Class Object
+	 *
+	 * @since   0.0.2
+	 * @return  object
+	 */
+	public static function get_object() {
+		
+		if ( NULL === self::$classobj )
+			self::$classobj = new self;
+		
+		return self::$classobj;
+	}
+	
 	/**
 	 * Create Taxonomy; define different variables and settings
-	 * Use Filter wpit_register_taxonomy for change params via Plugin
+	 * Use Filter `wp_control_taxonomy_register_taxonomy` for change params via Plugin
 	 * 
 	 * @since   0.0.1
 	 * @uses    wp_parse_args, apply_filters
@@ -18,25 +44,41 @@ class Wp_Control_Taxonomy {
 	 * @param   array  $args, different vars for taxonomy: ID, slug, array for labels, add columns
 	 * @return  void
 	 */
-	public function create_taxonomy( $post_type = FALSE, $args = FALSE ) {
+	public function __construct( $post_type = FALSE, $args = FALSE ) {
 		
 		if ( ! $post_type )
 			return NULL;
 		
 		$defaults = array(
-			'taxonomy'        => '',
-			'taxonomy_slug'   => '',
-			'taxonomy_labels' => array(),
-			'taxonomy_column' => FALSE
+			'textdomain'                        => 'wp_control_taxonomy',
+			'taxonomy'                          => '',
+			'taxonomy_slug'                     => '',
+			'taxonomy_labels'                   => array(),
+			'taxonomy_column'                   => FALSE,
+			'taxonomy_hierarchical'             => FALSE,
+			'terms'                             => FALSE,
+			'taxonomy_custom_hierarchical'      => FALSE,
+			'taxonomy_custom_hierarchical_type' => FALSE
 		);
 		
-		$args = wp_parse_args( $args, apply_filters( 'wpit_register_taxonomy', $defaults ) );
+		// Filter Hook for params
+		$args = wp_parse_args(
+			$args,
+			apply_filters( 'wp_control_taxonomy_register_taxonomy', $defaults )
+		);
 		
-		$this -> post_type       = $post_type;
-		$this -> taxonomy        = $args['taxonomy'];
-		$this -> taxonomy_slug   = $args['taxonomy_slug'];
-		$this -> taxonomy_labels = $args['taxonomy_labels'];
-		$this -> taxonomy_column = $args['taxonomy_column'];
+		$this->post_type                         = $post_type;
+		$this->textdomain                        = $args['textdomain'];
+		$this->taxonomy                          = $args['taxonomy'];
+		$this->taxonomy_slug                     = $args['taxonomy_slug'];
+		$this->taxonomy_labels                   = $args['taxonomy_labels'];
+		$this->taxonomy_column                   = $args['taxonomy_column'];
+		$this->taxonomy_hierarchical             = $args['taxonomy_hierarchical'];
+		$this->taxonomy_terms                    = $args['terms'];
+		$this->taxonomy_custom_hierarchical      = $args['taxonomy_custom_hierarchical'];
+		$this->taxonomy_custom_hierarchical_type = $args['taxonomy_custom_hierarchical_type'];
+		
+		$this->init();
 	}
 	
 	/**
@@ -48,17 +90,27 @@ class Wp_Control_Taxonomy {
 	 * @return  void
 	 */
 	public function init() {
+		
 		// Register the taxonomy.
 		add_action( 'init', array( $this, 'register_taxonomy' ) );
 		
+		// @ToDo: ändern, nur beim Aktivieren des Plugins register_activation_hook()
+		// after register tax is important
+		if ( $this->taxonomy_terms && is_array( $this->taxonomy_terms ) )
+			add_action( 'init', array( $this, 'insert_terms' ), 11 );
+		
 		// add to admin column, only important before WP 3.5
-		if ( $this -> taxonomy_column && version_compare( $GLOBALS['wp_version'], '3.5', '<' ) ) {
+		if ( $this->taxonomy_column && version_compare( $GLOBALS['wp_version'], '3.5', '<' ) ) {
 			// Add the taxonomy to the table columns.
 			add_action( 'manage_posts_custom_column', array( $this, 'manage_columns' ) );
 			
 			// Display the taxonomy in the table column.
-			add_filter( "manage_edit-{$this->post_type}_columns", array( $this, 'manage_column_titles' ) );
+			add_filter( "manage_edit-{$this->post_type}_columns", array( $this, 'manage_column_titles' ), 9 );
 		}
+		
+		// Possibility to list hierarchical
+		if ( $this->taxonomy_custom_hierarchical )
+			add_action( 'add_meta_boxes', array( $this, 'change_meta_box' ) );
 	}
 	
 	/**
@@ -69,9 +121,9 @@ class Wp_Control_Taxonomy {
 	 * @param   
 	 * @return  void
 	 */
-	public function get_text_domain() {
+	public function get_textdomain() {
 		
-		return parent::get_textdomain();
+		return apply_filters( 'wp_control_taxonomy_textdomain', $this->textdomain );
 	}
 	
 	/**
@@ -84,16 +136,18 @@ class Wp_Control_Taxonomy {
 	 */
 	public function register_taxonomy() {
 		
-		register_taxonomy( 
-			$this -> taxonomy, 
-			array( $this -> post_type ), 
+		register_taxonomy(
+			$this->taxonomy, 
+			array( $this->post_type ), 
 			array(
+				'hierarchical'          => $this->taxonomy_hierarchical,
 				'labels'                => apply_filters(
-					"ct_{$this->taxonomy}_labels", $this->taxonomy_labels
+					"wp_control_taxonomy_{$this->taxonomy}_labels",
+					$this->taxonomy_labels
 				),
 				'show_tagcloud'         => FALSE,
 				'show_ui'               => TRUE,
-				'show_admin_column'     => $this->taxonomy_column, // new since WP 3.5
+				'show_admin_column'     => $this->taxonomy_column,
 				'rewrite'               => array(
 					'slug' => $this->taxonomy_slug
 				),
@@ -102,13 +156,22 @@ class Wp_Control_Taxonomy {
 		);
 	}
 	
+	public function insert_terms() {
+		
+		foreach ( (array) $this->taxonomy_terms as $term ) {
+			
+			if ( ! term_exists( $term, $this->taxonomy ) )
+				wp_insert_term( $term , $this->taxonomy );
+		}
+		
+	}
+	
 	/**
 	 * Add this taxonomy to the array of column titles
 	 * 
 	 * @since   0.0.1
-	 * @uses    get_taxonomy
-	 * @param   array $columns, titles
-	 * @return  array $columns, new titles
+	 * @param   array columns titles
+	 * @return  array columns new titles
 	 */
 	function manage_column_titles( $columns ) {
 		
@@ -123,26 +186,123 @@ class Wp_Control_Taxonomy {
 	 * Create the callback for the column headers
 	 * 
 	 * @since   0.0.1
-	 * @uses    get_the_term_list
 	 * @param   array columns
 	 * @return  void
 	 */
 	function manage_columns( $column ) {
 		
 		switch( $column ) {
-			case $this -> taxonomy :
+			case $this->taxonomy :
 				
 				$tax = get_the_term_list( $GLOBALS['post']->ID, $this->taxonomy, '', ', ', '' );
 				
 				if ( ! empty( $tax ) )
 					echo $tax;
-				else 
-					_e( 'empty', $this->get_text_domain() );
+				else
+					_e( '—', $this->textdomain );
 				
 			break;
 		}
 	}
 	
+	/**
+	 * Change default Meta Box for hierarchical list
+	 * 
+	 * @since  12/12/2012  1.0.0
+	 * @param  String $post_type
+	 * @return void
+	 */
+	public function change_meta_box( $post_type ) {
+		
+		remove_meta_box( 'tagsdiv-' . $this->taxonomy, $this->post_type, 'side' );
+		$taxonomy = get_taxonomy( $this->taxonomy );
+		add_meta_box( 
+			'tagsdiv-' . $this->taxonomy,
+			$taxonomy->labels->name, 
+			array( $this, 'get_custom_meta_boxes' ),
+			$this->post_type,
+			'side'
+		);
+	}
+	
+	/**
+	 * Get the markup and date to use inside meta box
+	 * 
+	 * @since  12/12/2012  1.0.0
+	 * @param  Array  $post
+	 * @param  Array  $meta_box
+	 * @param  String $type      checkbox, radio
+	 * @return void
+	 */
+	public function get_custom_meta_boxes( $post, $meta_box, $type = 'checkbox' ) {
+		var_dump($meta_box);
+		$taxonomy = $this->taxonomy;
+		$tax      = get_taxonomy( $taxonomy );
+		$selected = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'ids' ) );
+		$name     = 'tax_input[' . $taxonomy . ']';
+		
+		// set type of input field
+		if ( $this->taxonomy_custom_hierarchical_type )
+			$type = $this->taxonomy_custom_hierarchical_type;
+		
+		if ( 'radio' === $type ) {
+			// Set up the taxonomy object and get terms  
+			$terms = get_terms( $taxonomy, array( 'hide_empty' => 0 ) );
+			
+			// Get popular terms
+			$popular  = get_terms( 
+				$taxonomy,
+				array( 
+					'orderby'      => 'count',
+					'order'        => 'DESC',
+					'number'       => 10,
+					'hierarchical' => FALSE
+				)
+			);
+			// Get current terms
+			$postterms = get_the_terms( $post->ID, $taxonomy );  
+			$current   = ( $postterms ? array_pop( $postterms ) : FALSE );  
+			$current   = ( $current ? $current->term_id : 0 );
+		}
+		?>
+		
+		<div id="taxonomy-<?php echo $taxonomy; ?>-all" class="categorydiv">
+			
+			<ul id="<?php echo $taxonomy; ?>checklist" class="list:<?php echo $taxonomy; ?> categorychecklist form-no-clear">
+				<?php
+				if ( 'checkbox' === $type ) {
+					
+					// get list as checkbox
+					wp_terms_checklist( 
+						$post->ID, array(
+							'taxonomy'      => $taxonomy,
+							'selected_cats' => $selected
+						)
+					);
+					
+				} else if ( 'radio' === $type ) {
+					
+					// get list as radio
+					foreach( $terms as $term ) {
+						echo '<li id="' . $taxonomy . '-' . $term->term_id . '">';
+						echo '<label class="selectit">';
+						echo '<input value="' . $term->name . '" type="radio" name="' 
+							. $name . '" id="in-' . $taxonomy . '-' . $term->term_id 
+							. '" ' . checked( $current, $term->term_id, FALSE ) . ' /> ' 
+							. $term->name;
+						echo '</label>';
+						echo '</li>';
+					}
+				} else {
+					
+					_e( 'No type - ToDo' );
+					
+				}
+				?>
+			</ul>
+		
+		</div>
+		<?php
+	}
+	
 } // end class
-
-$wp_control_taxonomy = new Wp_Control_Taxonomy;
